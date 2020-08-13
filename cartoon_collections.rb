a318ce27a616ea6758ed21c7dae76f178c98f961
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-13 10:01:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   number = 1
-array.each_with_index |name|
+array.each_with_index do |name|
 puts “ #{name}”
 number += 1
 end
