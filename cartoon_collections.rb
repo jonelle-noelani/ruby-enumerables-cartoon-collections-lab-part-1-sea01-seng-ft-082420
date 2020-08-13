090b726da1922046f8160db6774301af8c9310da
Jonelle Noelani Yacapin <jonelle_noelani@hotmail.com>
2020-08-13 10:10:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,13 +7,13 @@ puts "Hello #{name}!"
 end
 end
 
-def list_dwarves(array)
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
-  number = 1
-array.each_with_index do |name|
-  puts "#{number}. #{name}"
-number += 1 
-end
-end
+# def list_dwarves(array)
+#   # Use `each_with_index` to enumerate over the provided array
+#   #
+#   # Print a numbered list of each element
+#   number = 1
+# array.each_with_index do |name|
+#   puts "#{number}. #{name}"
+# number += 1 
+# end
+# end
